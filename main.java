40da8a825bd4c4320538d13025d8efcac693b103
majed <chaouchmajed@gmail.com>
2019-09-30 15:03:12 +0200

    ajout des menus

---
+++ main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Order {
+
+    public void displayMenu() {
+        System.out.println("Menu choix");
+	    System.out.println("Menu 1");
+        System.out.println("Menu 2");
+        System.out.println("Menu 3");
+        System.out.println("Choisissez SVP");
+    }
+
